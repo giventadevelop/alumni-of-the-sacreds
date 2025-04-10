d5de6e6fffedc11d66d751c950efcf1b0752e0ad
giventadevelop <giventa.develop@gmail.com>
2025-04-10 12:51:31 -0400

    amplify

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -8,7 +8,9 @@ import { Search } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 
-interface CommandDialogProps extends DialogProps { }
+interface CommandDialogProps extends DialogProps {
+  children: React.ComponentProps<typeof CommandPrimitive>['children'];
+}
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
